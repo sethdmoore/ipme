ccef3a9e753630388800caa600b1ac2e104acc85
Seth Moore <sethdmoore@github.com>
2015-10-07 01:29:12 -0400

    gofmt, procfile, godeps, go to 1.5

--- main.go
+++ main.go
@@ -1,40 +1,40 @@
-package main 
-import(
-    "os"
-    "fmt"
-    "strings"
-    "github.com/gin-gonic/gin"
+package main
+
+import (
+	"fmt"
+	"github.com/sethdmoore/ipme/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	"os"
+	"strings"
 )
 
 func strip_port() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        ip := strings.Split(c.ClientIP(), ":")[0]
-        //fmt.Println(strings.Split(ip_port, ":")[0])
-        fmt.Println(ip)
-        c.String(200, ip)
-    }
+	return func(c *gin.Context) {
+		ip := strings.Split(c.ClientIP(), ":")[0]
+		//fmt.Println(strings.Split(ip_port, ":")[0])
+		fmt.Println(ip)
+		c.String(200, ip)
+	}
 }
 
 func main() {
-    /*
-       FYI
-       gin.context doesn't work onwith ipv6
-    */
+	/*
+	   FYI
+	   gin.context doesn't work onwith ipv6
+	*/
 
-    port := "8080"
-    iface := os.Getenv("INTERFACE")
+	port := "8080"
+	iface := os.Getenv("INTERFACE")
 
-    r := gin.Default()
+	r := gin.Default()
 
-    r.GET("/", strip_port())
-    
-    port_env := os.Getenv("PORT")
+	r.GET("/", strip_port())
 
-    if (port_env != "") {
-        port = port_env
-    }
+	port_env := os.Getenv("PORT")
 
+	if port_env != "" {
+		port = port_env
+	}
 
-    fmt.Println(port)
-    r.Run(iface + ":" + port)
+	fmt.Println(port)
+	r.Run(iface + ":" + port)
 }
